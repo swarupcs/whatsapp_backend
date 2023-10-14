b5700b9e0d560cfba34157f65dc8634e1fe3e044
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-14 21:59:55 +0530

    Cross Env

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 //env variables
 const PORT = process.env.PORT || 8000;
-
+console.log(process.env.NODE_ENV);
 app.listen(PORT, ()=> {
     console.log(`server is listening at ${PORT}.....`);
 });
