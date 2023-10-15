6fde996e511514b82bb1a4ae698fabbb191f9583
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-15 21:18:46 +0530

    Handling uncaughtException, unhandledRejection errors and SIGTERM signal closing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,35 @@ const PORT = process.env.PORT || 8000;
 
 console.log(process.env.NODE_ENV);
 
-app.listen(PORT, ()=> {
+let server;
+server = app.listen(PORT, ()=> {
     logger.info(`server is listening at ${PORT}.....`);
+    // throw new Error("error in server. ");
+});
+
+//handle server errors
+
+const exitHandler=()=> {
+    if(server) {
+        logger.info("Server closed. ");
+        process.exit(1);
+    } else {
+        process.exit(1);
+    }
+}; 
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("uncaughtRejection", unexpectedErrorHandler);
+
+
+//SIGTERM
+process.on("SIGTERM", () => {
+    if(server) {
+        logger.info("Server closed. ");
+        process.exit(1);
+    } 
 });
