8e907f1bec3b1240e0d1a50cf6afd8ac476e15b6
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-15 10:49:20 +0530

    add middlewares ( morgan, helmet, mongoSanitize, cookieParser,compression, fileUpload, cors)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,54 @@
 import  express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from 'express-mongo-sanitize'
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//dotEnv config
+dotenv.config();
 
 //create express app
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("Hello from server")
+//Morgan
+if(process.env.NODE_ENV !== "production") {
+    app.use(morgan("dev"));
+}
+
+//Helmet
+app.use(helmet);
+
+//Parse json request url
+app.use(express.json());
+
+//Parse json request body
+app.use(express.urlencoded({ extended: true}));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//Enable coolie parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(fileUpload({
+    useTempFiles: true,
+}));
+
+//cors
+app.use(cors({
+    origin: 'http://localhost:3000',
+}))
+
+app.post('/test', (req,res) => {
+    res.send(req.body);
 })
 
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
 import app from "./app.js";
 
 
-//dotEnv config
-dotenv.config();
-
 //env variables
 const PORT = process.env.PORT || 8000;
+
 console.log(process.env.NODE_ENV);
+
 app.listen(PORT, ()=> {
     console.log(`server is listening at ${PORT}.....`);
 });
