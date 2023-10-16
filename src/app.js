8505f7ec63411a430686e29bef353da72fd6a5fb
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-16 09:38:35 +0530

    Handling httpError

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
 
 //dotEnv config
 dotenv.config();
@@ -20,7 +21,7 @@ if(process.env.NODE_ENV !== "production") {
 }
 
 //Helmet
-app.use(helmet);
+app.use(helmet());
 
 //Parse json request url
 app.use(express.json());
@@ -43,13 +44,27 @@ app.use(fileUpload({
 }));
 
 //cors
-app.use(cors({
-    origin: 'http://localhost:3000',
-}))
+app.use(cors())
 
 app.post('/test', (req,res) => {
-    res.send(req.body);
+    // res.status(409).json({ message: "there is a conflict"});
+    throw createHttpError.BadRequest("this route has an error")
 })
 
+app.use(async(req,res,next)=> {
+    next(createHttpError.NotFound("This route does not exist."));
+})
+
+
+//error handling
+app.use(async(err,req,res,next)=> {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        },
+    });
+});
 
 export default app;
