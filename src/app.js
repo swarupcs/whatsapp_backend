48f91d89519565a8a7400b4a97830b65b0abeb63
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-17 09:31:39 +0530

    mongoDb connection ,handle connection error, debug

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import  express from "express";
+import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
-import mongoSanitize from 'express-mongo-sanitize'
+import mongoSanitize from "express-mongo-sanitize";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 import createHttpError from "http-errors";
-import routes from './routes/index.js'
+import routes from "./routes/index.js";
 
 //dotEnv config
 dotenv.config();
@@ -16,57 +16,55 @@ dotenv.config();
 //create express app
 const app = express();
 
-//Morgan
-if(process.env.NODE_ENV !== "production") {
-    app.use(morgan("dev"));
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
-//Helmet
+//helmet
 app.use(helmet());
 
-//Parse json request url
+//parse json request url
 app.use(express.json());
 
-//Parse json request body
-app.use(express.urlencoded({ extended: true}));
+//parse json request body
+app.use(express.urlencoded({ extended: true }));
 
 //sanitize request data
 app.use(mongoSanitize());
 
-//Enable coolie parser
+//enable cookie parser
 app.use(cookieParser());
 
 //gzip compression
 app.use(compression());
 
 //file upload
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
-}));
+  })
+);
 
 //cors
-app.use(cors())
+app.use(cors());
 
 //api v1 routes
-app.use("/api/v1",routes)
+app.use("/api/v1", routes);
 
-
-
-
-app.use(async(req,res,next)=> {
-    next(createHttpError.NotFound("This route does not exist."));
-})
-
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist."));
+});
 
 //error handling
-app.use(async(err,req,res,next)=> {
-    res.status(err.status || 500);
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message,
-        },
-    });
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,77 @@
 import app from "./app.js";
-import logger from './configs/logger.config.js';
+import mongoose from "mongoose";
+import logger from "./configs/logger.config.js";
+
 
 
 //env variables
-const PORT = process.env.PORT || 8000;
+const DATABASE_URL = process.env.DATABASE_URL; 
+const PORT = process.env.PORT;
 
-console.log(process.env.NODE_ENV);
+// console.log('DATABASE_URL:', DATABASE_URL);
+// console.log('PORT:', PORT);
 
+//exit on mongodb error
+mongoose.connection.on('error',(err)=>  {
+    logger.error(`Mongodb connection error : ${err}`);
+    process.exit(1);
+})
+
+//mongodb debug mode
+if(process.env.NODE_ENV !== "production") {
+    // console.log("here");
+    mongoose.set("debug",true);
+}
+
+
+//exit on mognodb error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongodb connection error : ${err}`);
+  process.exit(1);
+});
+
+//mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+//mongodb connection
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    logger.info('Connected to Mongodb');
+}).catch((error) => {
+    logger.error('Error connecting to Mongodb:', error);
+});
 let server;
-server = app.listen(PORT, ()=> {
-    logger.info(`server is listening at ${PORT}.....`);
-    // throw new Error("error in server. ");
+
+server = app.listen(PORT, () => {
+  logger.info(`Server is listening at ${PORT}.`);
 });
 
 //handle server errors
 
-const exitHandler=()=> {
-    if(server) {
-        logger.info("Server closed. ");
-        process.exit(1);
-    } else {
-        process.exit(1);
-    }
-}; 
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
 
 const unexpectedErrorHandler = (error) => {
-    logger.error(error);
-    exitHandler();
+  logger.error(error);
+  exitHandler();
 };
 process.on("uncaughtException", unexpectedErrorHandler);
-process.on("uncaughtRejection", unexpectedErrorHandler);
-
+process.on("unhandledRejection", unexpectedErrorHandler);
 
 //SIGTERM
 process.on("SIGTERM", () => {
-    if(server) {
-        logger.info("Server closed. ");
-        process.exit(1);
-    } 
-}); 
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
+});
